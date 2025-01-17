9ad23e8ea88d765b9fa450b38cb04025f7da8463
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-01-17 23:22:39 +0800

    added sidebar seller

--- src/app/seller/[sellerId]/layout.tsx
+++ src/app/seller/[sellerId]/layout.tsx
@@ -1,22 +1,24 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import { redirect } from "next/navigation";
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from './_components/app-sidebar';
+import { useSeller } from '@/hooks/use-seller';
+import AppHeader from './_components/app-header';
 
 const SellerLayout = async ({ children }: { children: React.ReactNode }) => {
-//   const { seller, sellerId } = await useUser();
-//   if (!seller) {
-//     return redirect("/seller/account/login");
-//   }
+  const { seller } = await useSeller();
+  if (!seller) {
+    return redirect("/seller/account/login");
+  }
   return (
-    <div>
-      <p>Navbar</p>
-      <main className="px-40 py-10 grid md:grid-cols-6 grid-cols-1 gap-4">
-        <div className="col-span-1">
-          <p>Sidebar</p>
-        </div>
-        <div className="col-span-5">{children}</div>
-      </main>
-    </div>
+    <SidebarProvider>
+      <AppSidebar user={seller} />
+      <SidebarInset>
+        <AppHeader />
+        <main className="px-6 py-4">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
